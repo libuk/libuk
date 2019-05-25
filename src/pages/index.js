d47388375ac033446e20b60ee8172d2bb1a48689
Daniel Liburd <daniel.liburd@gmail.com>
2019-05-25 16:49:15 +0100

    post list - v1
    add date
    
    basic styling for post list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,48 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div class="header">
-      <h1 class="header-title">Daniel Liburd</h1>
-      <p class="header-desc">engineer @ yld</p>
-    </div>
-    <div class="links">
-      <div class="link-item-wrapper">
-        <a class="link-item" href="https://twitter.com/libuk_" target="_blank">
-          tweets
-        </a>
+function IndexPage() {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                path
+                date
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <div class="header">
+        <h1 class="header-title">Daniel Liburd</h1>
+        <p class="header-desc">engineer @ yld</p>
       </div>
-      <div class="link-item-wrapper">
-        <a class="link-item" href="https://github.com/libuk" target="_blank">
-          code
-        </a>
-      </div>
-    </div>
-  </Layout>
-)
+      <ul class="post-list">
+        {allMarkdownRemark.edges.map(({ node }) => (
+          <li class="post-list__item">
+            <a class="post-list__title" href={node.frontmatter.path}>
+              {node.frontmatter.title}
+            </a>
+            <span class="post-list__date">
+              {new Date(`${node.frontmatter.date}`).toLocaleDateString()}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
