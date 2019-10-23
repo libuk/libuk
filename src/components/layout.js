c9df935c135b93e93a4758ae1e0914c84596274c
Daniel Liburd <daniel.liburd@gmail.com>
2019-10-23 12:19:53 +0100

    cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,14 +25,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <Helmet>
             <link
               href="https://fonts.googleapis.com/css?family=Domine:700|Roboto:400,700&display=swap"
