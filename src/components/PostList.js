0bb6f91dd3be0d9ddd6f8d1251f3721fea1240b4
Daniel Liburd <daniel.liburd@gmail.com>
2019-11-02 23:39:42 +0000

    Add post list component

---
+++ src/components/PostList.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import "./PostList.css"
+
+const getDate = date =>
+  new Date(date).toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  })
+
+const PostList = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                path
+                date
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div class="post-list">
+      {allMarkdownRemark.edges.map(({ node }) => (
+        <a class="post-list__link" href={node.frontmatter.path}>
+          <article class="post-list__item">
+            <h2 class="post-list__title">{node.frontmatter.title}</h2>
+            <small class="post-list__date">
+              {getDate(node.frontmatter.date)}
+            </small>
+          </article>
+        </a>
+      ))}
+    </div>
+  )
+}
+
+export default PostList
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Intro from "../components/Intro"
 import SEO from "../components/seo"
+import PostList from "../components/PostList"
 
 function IndexPage() {
-  const { allMarkdownRemark, site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            description
-          }
-        }
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                title
-                path
-                date
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <Intro />
-      <ul class="post-list">
-        {allMarkdownRemark.edges.map(({ node }) => (
-          <li class="post-list__item">
-            <a class="post-list__title" href={node.frontmatter.path}>
-              {node.frontmatter.title}
-            </a>
-            <span class="post-list__date">
-              {new Date(`${node.frontmatter.date}`).toLocaleDateString()}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <PostList />
     </Layout>
   )
 }
