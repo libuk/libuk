fa753eabeb82ce0c0156b849eb69ce81ffe75708
Daniel Liburd <daniel.liburd@gmail.com>
2020-01-06 14:51:28 +0000

    Add fathom analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,12 @@ module.exports = {
         plugins: [`gatsby-remark-prismjs`],
       },
     },
+    {
+      resolve: "gatsby-plugin-fathom",
+      options: {
+        siteId: "OGXLKKBT",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
