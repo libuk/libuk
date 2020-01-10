61a9f17760e97546301f7e18a1077bbd57f94ae7
Daniel Liburd <daniel.liburd@gmail.com>
2020-01-10 17:30:42 +0000

    Add title tag to blog template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Helmet from "react-helmet"
 import Layout from "../components/layout"
 
 export default function Template({
@@ -9,6 +10,7 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
+      <Helmet title={frontmatter.title} />
       <div className="blog-post-container">
         <div className="blog-post">
           <h1>{frontmatter.title}</h1>
